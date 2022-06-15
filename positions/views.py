79d6c17f67a21f02e386e15223c0919d2be652fe
hatredholder <the.great.justice@bk.ru>
2022-06-15 13:51:22 +0300

    commented some code in a better way

--- positions/views.py
+++ positions/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    """View that gets data from the API and shows on template"""
+    """View that gets data from the API and shows it on template"""
     url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false"
     data = requests.get(url).json()
 
