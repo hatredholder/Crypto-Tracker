7878ba651927f2d5db031d9d110dca2475e2ff45
hatredholder <the.great.justice@bk.ru>
2022-06-15 17:52:42 +0300

    more. code. reformatting.

--- positions/views.py
+++ positions/views.py
@@ -4,9 +4,11 @@ from django.shortcuts import render
 
 def home_view(request):
     """View that gets data from the API and shows it on template"""
-    url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+
+    url = """https://api.coingecko.com/api/v3/coins/
+    markets?vs_currency=USD&order=market_cap_desc&
+    per_page=100&page=1&sparkline=false"""
     data = requests.get(url).json()
 
     context = {'data': data}
-
     return render(request, 'positions/main.html', context)
