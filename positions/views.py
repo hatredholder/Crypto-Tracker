ada86ad0c164c509c1c0e57d370c4982c56e2f2d
hatredholder <the.great.justice@bk.ru>
2022-07-29 16:29:28 +0300

    updated home_view logic

--- positions/views.py
+++ positions/views.py
@@ -1,14 +1,12 @@
 import requests
 from django.shortcuts import render
+from .models import Position
 
 
 def home_view(request):
     """View that gets data from the API and shows it on template"""
 
-    url = ("https://api.coingecko.com/api/v3/coins/"
-    "markets?vs_currency=USD&order=market_cap_desc&"
-    "per_page=100&page=1&sparkline=false")
-    data = requests.get(url).json()
+    data = Position.objects.all()
 
     context = {'data': data}
     return render(request, 'positions/main.html', context)
